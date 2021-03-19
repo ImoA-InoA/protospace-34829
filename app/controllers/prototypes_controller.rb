0680ef54f3829777d3ffdf1a52d7ff81cb7f8e35
ImoA-InoA <76636720+ImoA-InoA@users.noreply.github.com>
2021-03-19 14:49:05 +0900

    アクセスの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :move_to_index, except: [:index, :show]
 
   def index
@@ -49,9 +50,11 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end     
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index 
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
